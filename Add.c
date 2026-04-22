2a716e24d148c76d63255b31d6738e6cf68163e2
shahlakm07-sketch <shahlakm07@gmail.com>
2026-04-22 10:19:04 +0300

    Create Add.c

---
+++ Add.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    printf("Sum = %d", a + b);
+
+    return 0;
+}
+
+/*
+output
+Enter two numbers: 3 5
+Sum = 8
+ */
