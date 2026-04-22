8985e9c1ab033607c6cedc5764e33cac7254792f
shahlakm07-sketch <shahlakm07@gmail.com>
2026-04-22 10:23:02 +0300

    Create Largest.c

---
+++ Largest.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    if(a > b)
+        printf("Largest = %d", a);
+    else
+        printf("Largest = %d", b);
+
+    return 0;
+}
+
+/*
+output
+Enter two numbers: 39
+Largest = 9
+*/
