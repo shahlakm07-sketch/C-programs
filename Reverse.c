e4f94eddbc1cfca7584e5b20ffde7044d72803ee
shahlakm07-sketch <shahlakm07@gmail.com>
2026-04-22 11:00:41 +0300

    Update Reverse.c

--- Reverse.c
+++ Reverse.c
@@ -1,39 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Node structure
 struct Node {
     int data;
     struct Node* next;
 };
 
+// Function to reverse the linked list
+struct Node* reverseList(struct Node* head) {
+    struct Node* prev = NULL;
+    struct Node* current = head;
+    struct Node* next = NULL;
+
+    while (current != NULL) {
+        next = current->next;  
+        current->next = prev;  
+        prev = current;        
+        current = next;        
+    }
+    return prev; // prev becomes new head
+}
+
+// Helper: insert node at end
+void push(struct Node** head, int newData) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = newData;
+    newNode->next = *head;
+    *head = newNode;
+}
+
+// Helper: print list
+void printList(struct Node* head) {
+    while (head != NULL) {
+        printf("%d -> ", head->data);
+        head = head->next;
+    }
+    printf("NULL\n");
+}
+
 int main() {
-    struct Node *head = NULL, *temp, *prev = NULL, *current, *next;
-    int n, i;
+    struct Node* head = NULL;
 
-    printf("Enter number of nodes: ");
-    scanf("%d", &n);
+    // create list: 10 -> 20 -> 30 -> 40 -> NULL
+    push(&head, 40);
+    push(&head, 30);
+    push(&head, 20);
+    push(&head, 10);
 
-    for(i = 0; i < n; i++) {
-        temp = (struct Node*)malloc(sizeof(struct Node));
-        scanf("%d", &temp->data);
-        temp->next = head;
-        head = temp;
-    }
+    printf("Original list: ");
+    printList(head);
 
-    current = head;
-    while(current != NULL) {
-        next = current->next;
-        current->next = prev;
-        prev = current;
-        current = next;
-    }
+    head = reverseList(head);
 
     printf("Reversed list: ");
-    while(prev != NULL) {
-        printf("%d ", prev->data);
-        prev = prev->next;
-    }
+    printList(head);
 
     return 0;
 }
-
